0ff99e1ec9b17f8027d3098aa61c178d2b106036
Matteo Bordignon <matteo.bordignon@yahoo.it>
2023-09-25 17:50:36 +0200

    Fixed measurement eq, runs, bad results bc matrix close to singular

--- EKF/MeasurementEquations.m
+++ EKF/MeasurementEquations.m
@@ -4,13 +4,14 @@ function [y] = MeasurementEquations(x, hr, h0)
 % Maybe r and rdot need to be computed with theta and phi, How can i pass
 % them at each time step? the function cannot see the Workspace
 
-    y = zeros(9,1);
+    y = zeros(10,1);
 
     y(1:3) = x(1:3); % add noise maybe? TODO
     y(4:6) = x(4:6); % add noise maybe? 
     y(7) = log(hr/h0)/(log(x(3)/h0)) * norm(x(11:12));
     y(8) = atan(x(12)/x(11));
     y(9) = norm(x(1:3)*x(10));
+    y(10) = x(13:15)'*([x(11:12); 0] - x(4:6));
 
 end
 
--- EKF/StateEquations.m
+++ EKF/StateEquations.m
@@ -6,17 +6,17 @@ function [xkp1] = StateEquations(xk, u_sk, Ts, mk, mt_noL)
     g = [0,0,9.81]';
     m = mk + mt_noL*L; % this may not be correct, ask
 
-    xkp1 = zeros(15,1);
+    xkp1 = zeros(17,1);
     A = [m*eye(3), xk(1:3) ; xk(1:3)', 0];
-    B = [xk(13:16) + xk(17)*w_a/norm(w_a) + (mk+0.5*mt_noL*L)*g; 
+    B = [xk(13:15) + xk(17)*w_a/norm(w_a) + (mk+0.5*mt_noL*L)*g; 
         -xk(4:6)'*xk(1:3) + norm(xk(4:6))^2 + L*norm(xk(7:9))];
 
     xkp1(1:3) = xk(1:3) + xk(4:6).*Ts;                          %r
     xkp1(4:6) = xk(4:6) + xk(7:9).*Ts;                          %rd
     xkp1(7:10) = A\B;                                           %rdd, v
     xkp1(11:12) = xk(11:12);                                    %w_n
-    xkp1(13:16) = FLRot(xk(13:16),w_a,xk(18)*u_sk*Ts);          %F_L vec
-    xkp1(17) = xk(17);                                          %F_D
-    xkp1(18) = xk(18);                                          %cu
+    xkp1(13:15) = FLRot(xk(13:15),w_a,xk(17)*u_sk*Ts);          %F_L vec
+    xkp1(16) = xk(16);                                          %F_D
+    xkp1(17) = xk(17);                                          %cu
 end
 
